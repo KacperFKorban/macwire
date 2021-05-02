1250bc6bd4e57701587292e4f17e612d40488bc5
Kacper Korban <kacper.f.korban@gmail.com>
2021-05-02 17:40:56 +0200

    Make aggregated annotated modules work (1lvl depth)

--- scala3/src/main/scala/com/softwaremill/Wire.scala
+++ scala3/src/main/scala/com/softwaremill/Wire.scala
@@ -9,9 +9,22 @@ inline def wire[T]: T = ${wireImpl[T]}
 def wireImpl[T: Type](using Quotes): Expr[T] = {
   import quotes.reflect.*
 
+  case class FieldEntry(field: Symbol, fieldRef: Term)
+
   val wiredType = TypeRepr.of[T].typeSymbol
   val wiredDef = Symbol.spliceOwner.owner
   val wiredOwner = wiredDef.owner
+
+  def isMatchingField(paramType: TypeRepr, fieldEntry: FieldEntry) = {
+    if (wiredDef == fieldEntry.field) {
+      println(s"  Not checking: ${fieldEntry.field}")
+      false
+    } else {
+      println(s"  Checking: ${fieldEntry.field}")
+      val fieldType = Ref(fieldEntry.field).tpe.widen
+      fieldType <:< paramType
+    }
+  }
 
   val paramss = wiredType.primaryConstructor.paramSymss.flatten.map { param =>
     println(s"Looking for: $param")
@@ -21,27 +34,35 @@ def wireImpl[T: Type](using Quotes): Expr[T] = {
       wiredOwner.memberFields
     val annotatedModulesFields =
       wiredOwner.memberFields.map(_.tree)
-        .collect { case v: ValDef => v.tpt.tpe.typeSymbol }
-        .filter(_.annotations.exists(_.tpe.typeSymbol.name.contains("Module")))
-        .flatMap(_.memberFields)
+        .collect { case v: ValDef => v }
+        .filter(_.tpt.tpe.typeSymbol.annotations.exists(_.tpe.typeSymbol.name.contains("Module")))
+        .flatMap(f => f.tpt.tpe.typeSymbol.memberFields.map(field => (field, f.symbol)))
     
-    (ownFields ++ annotatedModulesFields).filter { field =>
-      if (wiredDef == field) {
-        println(s"  Not checking: $field")
-        false
-      } else {
-        println(s"  Checking: $field")
-        val fieldType = Ref(field).tpe.widen
-        fieldType <:< paramType
+    val fieldEntries = ownFields.map { field => FieldEntry(field, Ref(field)) } ++
+      annotatedModulesFields.map { (field, owner) =>
+        val ref = Select(Ref(owner), field)
+        FieldEntry(field, ref)
       }
-    } match {
+
+    fieldEntries.filter(isMatchingField(paramType, _)) match {
       case Nil => report.throwError(s"Cannot find value for type: $paramType")
       case l@(first :: second :: _) => report.throwError(s"For type: $paramType, found multiple values: $l")
-      case List(field) => Ref(field)
+      case List(fieldEntry) =>
+        fieldEntry.fieldRef
     }
   }
 
   val result = Apply(Select(New(TypeIdent(wiredType)), wiredType.primaryConstructor), paramss)
   println(result.show(using Printer.TreeAnsiCode))
   result.asExprOf[T]
+}
+
+inline def inspect[T](t: T) = ${inspectImpl[T]('t)}
+
+def inspectImpl[T: Type](t: Expr[T])(using Quotes): Expr[T] = {
+  import quotes.reflect.*
+
+  println(t.asTerm)
+
+  t
 }
